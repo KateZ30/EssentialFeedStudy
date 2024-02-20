044786e4bde78a180ef08d601e3756f8d7bf1a32
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-02-20 14:49:01 -0800

    Moved all the details to constructor injection

--- EssensialFeed/EssensialFeedTests/RemoteFeedLoaderTests.swift
+++ EssensialFeed/EssensialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,8 +9,16 @@ import XCTest
 @testable import EssensialFeed
 
 class RemoteFeedLoader {
-    func load(client: HTTPClient) {
-        client.get(from: URL(string: "https://a-url.com")!)
+    let client: HTTPClient
+    let url: URL
+    
+    init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    func load() {
+        client.get(from: url)
     }
 }
 
@@ -29,17 +37,19 @@ class HTTPClientSpy: HTTPClient {
 final class EssensialFeedTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() throws {
+        let url = URL(string: "https://a-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader()
+        _ = RemoteFeedLoader(url: url, client: client)
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
+        let url = URL(string: "https://a-url.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader()
+        let sut = RemoteFeedLoader(url: url, client: client)
         
-        sut.load(client: client)
+        sut.load()
         
         XCTAssertNotNil(client.requestedURL)
 
