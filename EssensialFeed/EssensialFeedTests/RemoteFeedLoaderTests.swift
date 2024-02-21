444f8779c31dd55ed776147a102f17570c172389
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-02-20 20:47:33 -0800

    create helper factory method to simplify test setup

--- EssensialFeed/EssensialFeedTests/RemoteFeedLoaderTests.swift
+++ EssensialFeed/EssensialFeedTests/RemoteFeedLoaderTests.swift
@@ -12,29 +12,29 @@ final class EssensialFeedTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() throws {
         let (_, client) = makeSUT()
-        
+
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
 
     func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
-        
+
         sut.load { _ in }
-        
+
         XCTAssertEqual(client.requestedURLs, [url])
     }
 
     func test_loadTwice_requestsDataFromURLTwice() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
-        
+
         sut.load { _ in }
         sut.load { _ in }
-        
+
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
-    
+
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
 
@@ -42,11 +42,11 @@ final class EssensialFeedTests: XCTestCase {
             client.complete(with: NSError(domain: "Test", code: 0), at: 0)
         }
     }
-    
+
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
         let samples = [199, 201, 300, 400, 500]
-        
+
         samples.enumerated().forEach { index, code in
             expect(sut, toCompleteWith: .failure(.invalidData)) {
                 client.complete(withStatusCode: 400, at: index)
@@ -63,9 +63,9 @@ final class EssensialFeedTests: XCTestCase {
         }
     }
 
-    func test_load_deliversNoItemsOn200HttpResponseWithEmptyJSONList() {
+    func test_load_deliversNoItemsOn200HttpResponseWithEmptyJSONItems() {
         let (sut, client) = makeSUT()
-        let emptyListJSON = Data("{\"items\": []}".utf8)
+        let emptyListJSON = makeItemsJson([])
 
         expect(sut, toCompleteWith: .success([])) {
             client.complete(withStatusCode: 200, data: emptyListJSON, at: 0)
@@ -73,11 +73,39 @@ final class EssensialFeedTests: XCTestCase {
 
     }
 
+    func test_load_deliversItemsOn200HttpResponseWithJSONList() {
+        let (sut, client) = makeSUT()
+        let item1 = makeItem(id: UUID(), imageURL: URL(string: "http://a-url.com")!)
+        let item2 = makeItem(id: UUID(), description: "a description", location: "a location", imageURL: URL(string: "http://another-url.com")!)
+        let itemsJSON = makeItemsJson([item1.json, item2.json])
+        let items = [item1.model, item2.model]
+
+        expect(sut, toCompleteWith: .success(items)) {
+            client.complete(withStatusCode: 200, data: itemsJSON, at: 0)
+        }
+    }
+
     // MARK: - Helpers
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
         return (sut, client)
+    }
+
+    private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedItem, json: [String: Any]) {
+        let item = FeedItem(id: id, description: description, location: location, imageURL: imageURL)
+        let json = [
+            "id": id.uuidString,
+            "description": description,
+            "location": location,
+            "image": imageURL.absoluteString
+        ].compactMapValues { $0 }
+
+        return (item, json)
+    }
+
+    private func makeItemsJson(_ items: [[String: Any]]) -> Data {
+        try! JSONSerialization.data(withJSONObject: ["items": items])
     }
 
     private func expect(_ sut: RemoteFeedLoader,
@@ -102,7 +130,7 @@ final class EssensialFeedTests: XCTestCase {
         func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
             messages.append((url, completion))
         }
-        
+
         func complete(with error: Error, at index: Int) {
             messages[index].completion(.failure(error))
         }
