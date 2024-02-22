02bbad7831a9a994282d317fb69f6e18a38d7739
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-02-21 19:32:54 -0800

    Make URLProtocolStub intercept all network requests for tests

--- EssensialFeed/EssensialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssensialFeed/EssensialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -51,7 +51,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
 
     // MARK: - Helpers
     private class URLProtocolStub: URLProtocol  {
-        private static var stubs: [URL: Stub] = [:]
+        private static var stub: Stub?
 
         private struct Stub {
             let data: Data?
@@ -60,7 +60,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
 
         static func stub(url: URL, data: Data?, response: HTTPURLResponse?, error: Error? = nil) {
-            Self.stubs[url] = Stub(data: data, response: response, error: error)
+            Self.stub = Stub(data: data, response: response, error: error)
         }
 
         static func startInterceptingRequests() {
@@ -69,12 +69,11 @@ final class URLSessionHTTPClientTests: XCTestCase {
 
         static func stopInterceptingRequests() {
             URLProtocol.unregisterClass(URLProtocolStub.self)
-            Self.stubs = [:]
+            Self.stub = nil
         }
 
         override class func canInit(with request: URLRequest) -> Bool {
-            guard let url = request.url else { return false }
-            return Self.stubs[url] != nil
+            return true
         }
 
         override class func canonicalRequest(for request: URLRequest) -> URLRequest {
@@ -82,7 +81,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
 
         override func startLoading() {
-            guard let url = request.url, let stub = Self.stubs[url] else { return }
+            guard let url = request.url, let stub = Self.stub else { return }
             if let data = stub.data {
                 client?.urlProtocol(self, didLoad: data)
             }
