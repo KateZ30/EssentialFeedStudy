79d2dc1707fabe51fb28ae7e42cc37d3f7f2f027
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-02-27 19:55:17 -0800

    Add RemoteFeedItem DTO to FeedItemMapper to decouple it from Feed Feature module

--- EssensialFeed/EssensialFeed/Feed API/FeedItemMapper.swift
+++ EssensialFeed/EssensialFeed/Feed API/FeedItemMapper.swift
@@ -7,35 +7,27 @@
 
 import Foundation
 
+struct RemoteFeedItem: Decodable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let image: URL
+}
+
 class FeedItemsMapper {
     struct Root: Decodable {
-        let items: [Item]
-
-        var feed: [FeedItem] {
-            items.map { $0.feedItem }
-        }
-    }
-
-    struct Item: Decodable {
-        public let id: UUID
-        public let description: String?
-        public let location: String?
-        public let image: URL
-
-        var feedItem: FeedItem {
-            FeedItem(id: id, description: description, location: location, imageURL: image)
-        }
+        let items: [RemoteFeedItem]
     }
 
     private static var OK_200: Int { 200 }
 
-    static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
         guard response.statusCode == OK_200,
               let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            return .failure(RemoteFeedLoader.Error.invalidData)
+            throw RemoteFeedLoader.Error.invalidData
         }
 
-        return .success(root.feed)
+        return root.items
     }
 
 }
