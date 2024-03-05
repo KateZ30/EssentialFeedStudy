726de44b974928c856f7000f226c6c28708973bb
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-03-04 19:18:04 -0800

    retrival from empty cache has no side effects

--- EssensialFeed/EssensialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssensialFeed/EssensialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -31,4 +31,23 @@ class CodableFeedStoreTests: XCTestCase {
 
         wait(for: [exp], timeout: 1.0)
     }
+
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = CodableFeedStore()
+        let exp = expectation(description: "Wait for retrieval")
+
+        sut.retrieve { result in
+            sut.retrieve { result in
+                switch result {
+                case .empty:
+                    break
+                default:
+                    XCTFail("Expected empty result, got \(result) instead")
+                }
+                exp.fulfill()
+            }
+        }
+
+        wait(for: [exp], timeout: 1.0)
+    }
 }
