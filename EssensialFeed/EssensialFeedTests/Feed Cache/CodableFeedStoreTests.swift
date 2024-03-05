50a4e208d6b13bf9e6cfa84ad7bb1d85461d28bb
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-03-04 19:38:57 -0800

    extract hardcoded store url from CodableFeedStore making it a constructor dependency

--- EssensialFeed/EssensialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssensialFeed/EssensialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -36,7 +36,13 @@ class CodableFeedStore {
         }
     }
 
-    private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("feed.store")
+    private let storeURL: URL
+
+    init(storeURL: URL) {
+        self.storeURL = storeURL
+    }
+
+
     func retrieve(completion: @escaping FeedStore.RetrivalCompletion) {
         guard let data = try? Data(contentsOf: storeURL) else {
             return completion(.empty)
@@ -131,7 +137,7 @@ class CodableFeedStoreTests: XCTestCase {
 
     // Mark: - Helpers
     func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
-        let sut = CodableFeedStore()
+        let sut = CodableFeedStore(storeURL: FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("feed.store"))
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
