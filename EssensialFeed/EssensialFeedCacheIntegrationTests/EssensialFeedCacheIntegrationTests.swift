364e5448a58fd8dcfe3131ece07a96c1c47d5068
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-04-12 18:31:43 -0700

    Created EssentialFeedCacheIntegrationTests target to separate imtegration tests (possibly slow) from unit/isolated tests (fast)

---
+++ EssensialFeed/EssensialFeedCacheIntegrationTests/EssensialFeedCacheIntegrationTests.swift
@@ -0,0 +1,11 @@
+//
+//  EssensialFeedCacheIntegrationTests.swift
+//  EssensialFeedCacheIntegrationTests
+//
+//  Created by Kate Zemskova on 4/12/24.
+//
+
+import XCTest
+
+final class EssensialFeedCacheIntegrationTests: XCTestCase {
+}
