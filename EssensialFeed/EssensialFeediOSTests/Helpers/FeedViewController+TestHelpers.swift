68ae070924e42978c8066f2678da4594fcfc94a6
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-04-24 19:20:14 -0700

    Added helper methods to simulate view apperance in iOS17 that introduced breaking changes that made it harder to use and test UIRefreshControl.

---
+++ EssensialFeed/EssensialFeediOSTests/Helpers/FeedViewController+TestHelpers.swift
@@ -0,0 +1,58 @@
+//
+//  FeedViewController+TestHelpers.swift
+//  EssensialFeediOSTests
+//
+//  Created by Kate Zemskova on 4/24/24.
+//
+
+import UIKit
+
+extension FeedViewController {
+    func simulateAppearance() {
+        if !isViewLoaded {
+            loadViewIfNeeded()
+            prepareForFirstAppearance()
+        }
+
+        beginAppearanceTransition(true, animated: false)
+        endAppearanceTransition()
+    }
+
+    func simulateUserInitiatedFeedReload() {
+        refreshControl?.simulatePullToRefresh()
+    }
+
+    var isShowingLoadingIndicator: Bool {
+        return refreshControl?.isRefreshing == true
+    }
+
+    private func prepareForFirstAppearance() {
+        replaceRefreshControlWithSpyForiOS17Support()
+    }
+
+    private func replaceRefreshControlWithSpyForiOS17Support() {
+        let spyRefreshControl = UIRefreshControlSpy()
+
+        refreshControl?.allTargets.forEach { target in
+            refreshControl?.actions(forTarget: target, forControlEvent: .valueChanged)?.forEach { action in
+                spyRefreshControl.addTarget(target, action: Selector(action), for: .valueChanged)
+            }
+        }
+
+        refreshControl = spyRefreshControl
+    }
+}
+
+private class UIRefreshControlSpy: UIRefreshControl {
+    private var _isRefreshing = false
+
+    override var isRefreshing: Bool { _isRefreshing }
+
+    override func beginRefreshing() {
+        _isRefreshing = true
+    }
+
+    override func endRefreshing() {
+        _isRefreshing = false
+    }
+}
