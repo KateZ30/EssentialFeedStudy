9f6c6b57af43e420da17cd2cd519f01b32ee61a0
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-05-03 15:54:00 -0700

    Moving `FeedViewModel` and `FeedLoadingViewModel` to separate files

---
+++ EssensialFeed/EssensialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssensialFeediOS
+//
+//  Created by Kate Zemskova on 5/3/24.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssensialFeed/EssensialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssensialFeediOS
+//
+//  Created by Kate Zemskova on 5/3/24.
+//
+
+import Foundation
+import EssensialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
