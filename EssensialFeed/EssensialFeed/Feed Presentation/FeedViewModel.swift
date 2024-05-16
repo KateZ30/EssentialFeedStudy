5739bbf3e081c53a693f0c63b88fefd8ad6e9520
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-05-16 15:04:42 -0700

    Move production types to `EssensialFeed` module

---
+++ EssensialFeed/EssensialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssensialFeed
+//
+//  Created by Kate Zemskova on 5/16/24.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
