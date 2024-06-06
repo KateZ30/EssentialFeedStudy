a77ad89012d03c18a0f1d7424cce1c8e684e92ff
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-06-05 20:17:32 -0700

    Up `FeedStore` version and add new `data` field to `ManagedFeedImage`

--- EssensialFeed/EssensialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
+++ EssensialFeed/EssensialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -12,5 +12,6 @@ final class ManagedFeedImage: NSManagedObject {
 	@NSManaged var imageDescription: String?
 	@NSManaged var location: String?
 	@NSManaged var url: URL
+    @NSManaged var data: Data?
 	@NSManaged var feed: ManagedFeed
 }
