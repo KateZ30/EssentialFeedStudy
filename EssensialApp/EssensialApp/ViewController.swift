b14fbb91151cd7e8033f09d51cc7004b3facaba6
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-06-06 19:58:43 -0700

    Add empty` EssensialApp` project

---
+++ EssensialApp/EssensialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssensialApp
+//
+//  Created by Kate Zemskova on 6/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
