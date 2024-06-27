86a3b6526fb5aa0ee51fe5d2e3993741ec561963
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-06-27 14:47:10 -0700

    Remove unused debugging code

--- EssensialApp/EssensialApp/AppDelegate.swift
+++ EssensialApp/EssensialApp/AppDelegate.swift
@@ -16,17 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        let configuration = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-
-        #if DEBUG
-        configuration.delegateClass = DebuggingSceneDelegate.self
-        #endif
-
-        return configuration
-    }
-
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
