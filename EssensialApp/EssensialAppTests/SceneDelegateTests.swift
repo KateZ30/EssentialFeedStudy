1a273b47dfa27539c19a8f9c52f042fe2e62a10b
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-06-27 13:38:34 -0700

    Configure feed navigation as window root view controller

--- EssensialApp/EssensialAppTests/SceneDelegateTests.swift
+++ EssensialApp/EssensialAppTests/SceneDelegateTests.swift
@@ -5,4 +5,22 @@
 //  Created by Kate Zemskova on 6/27/24.
 //
 
-import Foundation
+import XCTest
+import EssensialFeediOS
+@testable import EssensialApp
+
+class SceneDelegateTests: XCTest {
+    func  test_sceneWillConnectToSession_configuresRootViewController() {
+        let sut = SceneDelegate()
+        sut.window = UIWindow()
+
+        sut.configureWindow()
+
+        let root = sut.window?.rootViewController
+        let rootNavigation = root as? UINavigationController
+        let topController = rootNavigation?.topViewController
+
+        XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root))")
+        XCTAssertTrue(topController is FeedViewController, "Expected a feed view controller as top view controller, got \(String(describing: topController))")
+    }
+}
