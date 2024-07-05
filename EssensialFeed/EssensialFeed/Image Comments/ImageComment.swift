b57161372770b10044a8356f461939a948d13a12
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-07-05 16:11:26 -0700

    Add `ImageComment` DTO

---
+++ EssensialFeed/EssensialFeed/Image Comments/ImageComment.swift
@@ -0,0 +1,22 @@
+//
+//  ImageComment.swift
+//  EssensialFeed
+//
+//  Created by Kate Zemskova on 7/5/24.
+//
+
+import Foundation
+
+public struct ImageComment: Equatable {
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
