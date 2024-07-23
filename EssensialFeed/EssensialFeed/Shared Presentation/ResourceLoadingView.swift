2a471c5af5d64aca12b36bed966e5613f36773e7
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-07-22 17:15:51 -0700

    Rename `FeedLoadingView` and `FeedLoadingViewModel` to `ResourceLoadingView` and `ResourceLoadingViewModel`  and move to Shared Presentation module

---
+++ EssensialFeed/EssensialFeed/Shared Presentation/ResourceLoadingView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceLoadingView.swift
+//  EssensialFeed
+//
+//  Created by Kate Zemskova on 7/22/24.
+//
+
+import Foundation
+
+public protocol ResourceLoadingView {
+    func display(_ viewModel: ResourceLoadingViewModel)
+}
--- EssensialFeed/EssensialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
+++ EssensialFeed/EssensialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
@@ -69,7 +69,7 @@ class LoadResourcePresenterTests: XCTestCase {
         return localizedString
     }
 
-    private class ViewSpy: FeedErrorView, FeedLoadingView, ResourceView {
+    private class ViewSpy: FeedErrorView, ResourceLoadingView, ResourceView {
         typealias ResourceViewModel = String
         enum Message: Hashable {
             case display(errorMessage: String?)
@@ -82,7 +82,7 @@ class LoadResourcePresenterTests: XCTestCase {
             messages.insert(.display(errorMessage: viewModel.message))
         }
 
-        func display(_ viewModel: FeedLoadingViewModel) {
+        func display(_ viewModel: ResourceLoadingViewModel) {
             messages.insert(.display(isLoading: viewModel.isLoading))
         }
 
