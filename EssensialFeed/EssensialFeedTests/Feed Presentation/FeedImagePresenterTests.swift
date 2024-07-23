c0bf02192f4756f66148b3674a62f31d97bea8bd
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-07-22 18:16:54 -0700

    `LoadResourcePresenter` displays error on mapper error

---
+++ EssensialFeed/EssensialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -0,0 +1,8 @@
+//
+//  FeedImagePresenterTests.swift
+//  EssensialFeedTests
+//
+//  Created by Kate Zemskova on 7/22/24.
+//
+
+import Foundation
--- EssensialFeed/EssensialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
+++ EssensialFeed/EssensialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
@@ -47,6 +47,20 @@ class LoadResourcePresenterTests: XCTestCase {
         ])
     }
 
+    func test_didFinishLoadingWithMapperError_displaysLocalizedMessageAndStopsLoading() {
+        let (sut, view) = makeSUT(mapper: { resource in
+            throw anyNSError()
+        })
+
+        sut.didFinishLoading(with: "resource")
+
+        XCTAssertEqual(view.messages, [
+            .display(errorMessage: localized("GENERIC_CONNECTION_ERROR")),
+            .display(isLoading: false)
+        ])
+    }
+
+
     // MARK: - Helpers
     private typealias SUT = LoadResourcePresenter<String, ViewSpy>
     private func makeSUT(mapper: @escaping SUT.Mapper = { _ in "any" },
