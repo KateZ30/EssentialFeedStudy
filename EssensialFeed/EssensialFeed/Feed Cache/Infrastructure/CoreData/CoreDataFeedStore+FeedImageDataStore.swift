b43baa92768dc5f187580e93225e9aed8a687b15
Kate Zemskova <ekaterina.zemskova@gmail.com>
2024-08-29 16:29:54 -0700

    rename method to clarify intent

--- EssensialFeed/EssensialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
+++ EssensialFeed/EssensialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension CoreDataFeedStore: FeedImageDataStore {
     public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertResult) -> Void) {
-        perform { context in
+        performAsync { context in
             completion(Result {
                 guard let image = try ManagedFeedImage.first(with: url, in: context) else { return }
                 image.data = data
@@ -20,7 +20,7 @@ extension CoreDataFeedStore: FeedImageDataStore {
     }
 
     public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrieveResult) -> Void) {
-        perform { context in
+        performAsync { context in
             completion(Result {
                 try ManagedFeedImage.first(with: url, in: context)?.data
             })
--- EssensialFeed/EssensialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
+++ EssensialFeed/EssensialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension CoreDataFeedStore: FeedStore {
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        perform { context in
+        performAsync { context in
             completion(Result {
                 try ManagedFeed.find(in: context).map {
                     return CachedFeed(feed: $0.localFeedImages, timestamp: $0.timestamp)
@@ -19,7 +19,7 @@ extension CoreDataFeedStore: FeedStore {
     }
 
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        perform { context in
+        performAsync { context in
             completion(Result {
                 try ManagedFeed.createUniqueFeed(in: context, timestamp: timestamp, feed: feed)
                 try context.save()
@@ -28,7 +28,7 @@ extension CoreDataFeedStore: FeedStore {
     }
 
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        perform { context in
+        performAsync { context in
             completion(Result {
                 try ManagedFeed.find(in: context)
                     .map(context.delete)
